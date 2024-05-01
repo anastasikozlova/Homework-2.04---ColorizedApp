04e63dc2119da44c5aca66cab686935cf0720fde
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-05-01 14:52:14 +0200

    Initial Commit

---
+++ Homework 2.04 - ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.04 - ColorizedApp
+//
+//  Created by Анастасия Козлова on 01.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
